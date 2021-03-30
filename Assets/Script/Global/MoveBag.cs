a7e6a491d680e7ea8480c9a559e59cb8596eb2bd
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:12:58 +0100

    Create MoveBag.cs

---
+++ Assets/Script/Global/MoveBag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class MoveBag : MonoBehaviour, IDragHandler {
+    public Canvas canvas;
+    RectTransform currentRect;
+
+    public void OnDrag(PointerEventData eventData) {
+        currentRect.anchoredPosition += eventData.delta;
+    }
+
+    void Awake() {
+        currentRect = GetComponent<RectTransform>();
+    }
+
+    public void clickCloseBtn(){
+        PlayerController.Instance.IsOpenbag = false;
+        gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+}
