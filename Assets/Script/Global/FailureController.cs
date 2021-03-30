eb2b8f405b872dc345e4889b52b7a2a80b347c34
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:12:14 +0100

    Create FailureController.cs

---
+++ Assets/Script/Global/FailureController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FailureController : MonoBehaviour {
+    
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            PlayerController.Instance.DestoryGameObject();
+            SceneManager.LoadScene(0);
+        }
+    }
+}
