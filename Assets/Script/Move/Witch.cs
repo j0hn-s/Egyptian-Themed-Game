eddcbf63785acf54b163977fa26c916b63ea3f39
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:16:45 +0100

    Create Witch.cs

---
+++ Assets/Script/Move/Witch.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Witch : Enemy {
+    private float moveRate = 2.0f;
+    private float moveTimer;
+    private float shootRate = 2.0f;
+    private float shootTimer;
+
+    private Animator amim;
+    public GameObject projectile;
+
+
+    [SerializeField] private float minX, maxX, minY, maxY;
+
+
+    private void Start() {
+        amim = GetComponent<Animator>();
+    }
+    protected override void Introduction() {
+        //base.Introduction();
+    }
+    protected override void TurnDirection() {
+        base.TurnDirection();
+    }
+    protected override void FollowPlayer() {
+        //base.FollowPlayer();
+        RandomMove();
+    }
+    private void RandomMove() {
+        moveTimer += Time.deltaTime;
+        if (moveTimer > moveRate) {
+            transform.position = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0);
+            moveTimer = 0;
+        }
+        TurnDirection();
+
+    }
+
+    protected override void Attack() {
+        base.Attack();
+        shootTimer += Time.deltaTime;
+        if (shootTimer > shootRate) {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            shootTimer = 0;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
