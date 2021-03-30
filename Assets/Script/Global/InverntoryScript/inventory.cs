d27bffa941f0f825eb007eef0f1aa85a0595924a
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:12:28 +0100

    Create inventory.cs

---
+++ Assets/Script/Global/InverntoryScript/inventory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Inventory",menuName ="Inventory/New Inventory")]
+public class inventory : ScriptableObject
+{
+    #region Singleton
+
+	public static inventory instance;
+
+	void Awake ()
+	{
+		if (instance != null)
+		{
+			Debug.LogWarning("More than one instance of Inventory found!");
+			return;
+		}
+
+		instance = this;
+	}
+
+	#endregion
+    public List<item> itemList = new List<item>();
+
+
+
+}
