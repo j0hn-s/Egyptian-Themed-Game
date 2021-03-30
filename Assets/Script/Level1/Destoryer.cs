b62c51795751480d70e81c5d97bbf037d6dad22f
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:32:11 +0100

    Create Destoryer.cs

---
+++ Assets/Script/Level1/Destoryer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destoryer : MonoBehaviour {
+    // public GameObject talkUI;
+
+    public TalkButton1 talkShow;
+
+    private void Start() {
+        talkShow = GameObject.FindGameObjectWithTag("NPC").GetComponent<TalkButton1>();
+    }
+    private void Update() {
+
+        if (talkShow.chooseRight == true) {
+            Destroy(gameObject);
+        }
+    }
+
+}
