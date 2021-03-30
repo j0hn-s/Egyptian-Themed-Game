7241a3fcb5d1e85efe8b7c34850f6a9c10cbc999
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:12:49 +0100

    Create Slot.cs

---
+++ Assets/Script/Global/InverntoryScript/Slot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Slot : MonoBehaviour {
+    public int slotID; //Space ID is equal to item ID
+    public item slotItem;
+    public Image slotImage;
+
+    public Text slotnume;
+    public string slotInfo;
+    public int slotType;
+
+    public GameObject itemInslot;
+
+    private string equipmentString;
+
+    public void ItemOnClicked() {
+        InventoryManage.UpdateItemInfo(slotInfo);
+    }
+
+    public void SetuoSlot(item item) {
+        if (item == null) {
+            itemInslot.SetActive(false);
+            return;
+        }
+        slotImage.sprite = item.itemImage;
+        slotnume.text = item.itemHeld.ToString();
+        slotInfo = item.itemInfo;
+        slotType = (int)item.equipSlot;
+        equipmentString = item.equipmentString;
+    }
+
+    public void useItem() {
+        switch (slotType) {
+            case 0:
+                InventoryManage.useItem(0, equipmentString);
+                break;
+            case 1:
+                InventoryManage.useItem(1, equipmentString);
+                break;
+            case 2:
+                InventoryManage.useItem(2, equipmentString);
+                break;
+            case 3:
+                InventoryManage.useItem(3, equipmentString);
+                break;
+        }
+    }
+
+
+}
