3a5f22f9d785c345f823c64976ca5c4d21847a4b
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:12:42 +0100

    Create IteminWorld.cs

---
+++ Assets/Script/Global/InverntoryScript/IteminWorld.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IteminWorld : MonoBehaviour
+{
+
+    public item thisItem;
+    public inventory playerInventory;
+    
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            AddNewItem();
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddNewItem()
+    {
+        if(!playerInventory.itemList.Contains(thisItem))
+        {
+            for(int i = 0; i < playerInventory.itemList.Count;i++)
+            {
+                if(playerInventory.itemList[i] == null)
+                {
+                    playerInventory.itemList[i] = thisItem;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            thisItem.itemHeld += 1;
+        }
+
+        InventoryManage.RefreshItem();
+    }
+
+}
