694f5256b6f2f658341323f4454f6084ffa8847c
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:15:32 +0100

    Create Crystal.cs

---
+++ Assets/Script/Level2/Crystal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal : Enemy
+{
+    public float x;
+    public float y;
+    public GameObject winning;
+    public GameObject button;
+
+
+
+
+    private void Awake()
+    {
+
+    }
+
+    protected override void Introduction()
+    {
+        //base.Introduction();
+    }
+
+    protected override void TurnDirection()
+    {
+        // base.TurnDirection();
+    }
+
+    protected override void Attack()
+    {
+        //base.Attack();
+    }
+
+    protected override void Death()
+    {
+        Instantiate(Coin, transform.position + new Vector3(y, x), Quaternion.identity);
+        if (gameObject != null)
+        {
+            winning.SetActive(true);
+            button.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+
+}
