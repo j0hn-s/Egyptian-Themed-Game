df6a5361b1799dd2f42ef2fab6a4298f9c49ab26
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:12:09 +0100

    Create DestoryEffect.cs

---
+++ Assets/Script/Global/DestoryEffect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestoryEffectObject", 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void DestoryEffectObject(){
+        Destroy(gameObject);
+    }
+
+}
