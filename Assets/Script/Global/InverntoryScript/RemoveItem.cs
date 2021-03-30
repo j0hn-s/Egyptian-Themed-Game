7cebc509d9fa2591c9c3b7bc1392d333658e036e
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:12:45 +0100

    Create RemoveItem.cs

---
+++ Assets/Script/Global/InverntoryScript/RemoveItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveItem : MonoBehaviour
+{
+    public Transform thisItem;
+    public inventory playerInventory;
+    private int Itemid;
+
+    public void Remove()
+    {
+        thisItem = transform.parent;
+        Itemid = thisItem.GetComponent<Slot>().slotID;
+        string equipmentString = playerInventory.itemList[Itemid].equipmentString;
+        EquipmentType equipmentType = playerInventory.itemList[Itemid].equipSlot;
+        playerInventory.itemList[Itemid] = null;
+        Spawn.instance.SpawnItem(equipmentType,equipmentString);
+        InventoryManage.RefreshItem();
+        
+    }
+
+
+
+
+}
