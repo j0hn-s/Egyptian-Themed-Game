b3f5d91fbf9b13d9b96b208abff1cb3945201cf2
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:13:09 +0100

    Create PauseButtonController.cs

---
+++ Assets/Script/Global/PauseButtonController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseButtonController : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    public GameObject WarningMessage;
+
+    public void PauseGame() {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+    public void ResumeGame() {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void showWarningMessage() {
+        WarningMessage.SetActive(true);
+    }
+
+    public void hideWarningMessage() {
+        WarningMessage.SetActive(false);
+    }
+
+    public void backToMainMenu() {
+        PlayerController.Instance.DestoryGameObject();
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
