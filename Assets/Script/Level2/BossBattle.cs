86e73625700bc80ceb8a4057b8000a98057ba183
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:15:29 +0100

    Create BossBattle.cs

---
+++ Assets/Script/Level2/BossBattle.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBattle : MonoBehaviour
+{
+    [SerializeField] private Triggers colliderTrigger;
+   
+
+    void Start()
+    {
+        colliderTrigger.OnPlayerEnterTrigger += Triggers_OnPlayerEnterTrigger;
+    }
+
+    private void Triggers_OnPlayerEnterTrigger(object sender, System.EventArgs e)
+    {
+        StartBattle();
+        colliderTrigger.OnPlayerEnterTrigger -= Triggers_OnPlayerEnterTrigger;
+    
+    }
+
+    private void StartBattle()
+    {
+        Debug.Log("StartBattle");
+    
+    
+    }
+
+    
+}
