bbbb07e413fe952d97caad026b5db8a914ca29f6
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:12:22 +0100

    Create HideColliderColor.cs

---
+++ Assets/Script/Global/HideColliderColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HideColliderColor : MonoBehaviour {
+    private TilemapRenderer tilemapRenderer;
+
+    private void Awake() {
+        tilemapRenderer = GetComponent<TilemapRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        tilemapRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
