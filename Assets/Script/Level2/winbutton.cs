acc616a187c280d48adf4b8eab50c59a53bce759
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:15:55 +0100

    Create winbutton.cs

---
+++ Assets/Script/Level2/winbutton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class winbutton : MonoBehaviour
+{
+
+    public void backToMainMenu()
+    {
+        PlayerController.Instance.DestoryGameObject();
+        SceneManager.LoadScene(0);
+    }
+
+}
