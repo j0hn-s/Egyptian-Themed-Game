a80e5feaa71c9bb882016dacb64929146a83f602
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:15:08 +0100

    Create TalkButton1.cs

---
+++ Assets/Script/Level1/TalkButton1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkButton1 : MonoBehaviour {
+    public GameObject Button;
+    public GameObject talkUI;
+
+    public bool chooseRight = false;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            Button.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            Button.SetActive(false);
+        }
+    }
+
+    private void Update() {
+        if (Button.activeSelf && Input.GetKeyDown(KeyCode.R)) {
+            talkUI.SetActive(true);
+            chooseRight = true;
+        }
+        if (chooseRight) {
+            Invoke("destoryTalk", 8f);
+        }
+    }
+
+    void destoryTalk() {
+        Destroy(gameObject);
+    }
+
+}
