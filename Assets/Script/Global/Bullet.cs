ebd70f1b736435fbaea6e60b5c3b800b53e104ce
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:11:57 +0100

    Create Bullet.cs

---
+++ Assets/Script/Global/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public Animator animator;
+
+    float moveSpeed = 5f;
+    Rigidbody2D rb;
+
+    PlayerController target;
+    Vector2 moveDirection;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        target = PlayerController.Instance;
+        if (target){
+            moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+            Destroy(gameObject, 3f);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.name.Equals("Player")) {
+            animator.SetBool("Death", true);
+            Destroy(gameObject, 0.5f);
+        }
+    }
+}
