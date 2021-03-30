98bc509e9227090dc59d7a9b883bfed5ddcad553
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:12:02 +0100

    Create CameraController.cs

---
+++ Assets/Script/Global/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public static CameraController instance;
+
+    public float speed;
+
+    private Transform target;
+
+    private void Awake() {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        target = PlayerController.Instance.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (target != null) {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), speed * Time.deltaTime);
+        }
+    }
+
+    public void ChangeTarget(Transform newTarget) {
+        target = newTarget;
+    }
+
+}
