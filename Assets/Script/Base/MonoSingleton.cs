7c87162b4a9651161311ebc748d66230612741c7
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:11:44 +0100

    Create MonoSingleton.cs

---
+++ Assets/Script/Base/MonoSingleton.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * @Date: 2020-11-20 19:13:47
+ * @Author: Zilin Zhang
+ * @LastEditors: Zilin Zhang
+ * @LastEditTime: 2020-12-01 06:55:04
+ * @FilePath: /SE_CW2_Group8_Yellow/Assets/Script/Base/MonoSingleton.cs
+ * @Description: The base class of Mono Singleton Object
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : Component {
+    private static T _instance;
+
+    public static T Instance {
+        get {
+            if (_instance == null) {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null) {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public virtual void Awake() {
+        if (_instance == null) {
+            _instance = this as T;
+            DontDestroyOnLoad(this.gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void DestoryGameObject(){
+        Destroy(this.gameObject);
+    } 
+
+    public void DestoryScriptInstance(){
+        Destroy(this);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
