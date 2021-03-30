df8ea717725d1e1787c662afaa907990dfe231c9
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:16:27 +0100

    Create Mummy.cs

---
+++ Assets/Script/Move/Mummy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mummy : Enemy {
+    protected override void Introduction() {
+        //base.Introduction();
+    }
+
+}
