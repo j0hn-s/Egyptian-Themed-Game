4212c8b076cec5a44ce2acc03e67a28828861717
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:16:35 +0100

    Create Slime.cs

---
+++ Assets/Script/Move/Slime.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime : Enemy {
+    protected override void Introduction() {
+        //base.Introduction();
+    }
+    
+}
