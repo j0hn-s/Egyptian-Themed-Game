3947b05b25255e6344614a04a506eee80516f16b
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-30 17:15:36 +0100

    Create EntrancePoint.cs

---
+++ Assets/Script/Level2/EntrancePoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntrancePoint : MonoBehaviour
+{
+
+    [SerializeField] private string entrancePwd;
+
+    [SerializeField] private GameObject failureDialog;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerController.Instance.scenePwd == entrancePwd) {
+            PlayerController.Instance.transform.position = transform.position;
+            PlayerController.Instance.FailureDialog = failureDialog;
+        } else {
+            Debug.Log("Wrong password");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
