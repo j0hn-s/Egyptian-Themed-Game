572e3f8584f63e77326123b8524b46c08ff5f851
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-31 11:32:17 +0100

    Create Triggers.cs

---
+++ Assets/Triggers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Triggers : MonoBehaviour
+{
+    public event EventHandler OnPlayerEnterTrigger;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        PlayerController player = collider.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            OnPlayerEnterTrigger.Invoke(this, EventArgs.Empty);
+        
+        }
+    
+    }
+}
