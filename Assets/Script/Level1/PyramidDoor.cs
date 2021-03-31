c45d0a68f39e190d0090acc970134cc289a0d11c
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-31 11:34:53 +0100

    Create PyramidDoor.cs

---
+++ Assets/Script/Level1/PyramidDoor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PyramidDoor : MonoBehaviour {
+    
+    public GameObject enterDialog;
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            enterDialog.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            enterDialog.SetActive(false);
+        }
+    }
+}
