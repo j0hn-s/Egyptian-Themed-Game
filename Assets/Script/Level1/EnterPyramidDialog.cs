815a320be2bc4f9ba2e8a47045c9f60681a61880
johnlsmith80 <68287860+johnlsmith80@users.noreply.github.com>
2021-03-31 11:34:58 +0100

    Create EnterPyramidDialog.cs

---
+++ Assets/Script/Level1/EnterPyramidDialog.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnterPyramidDialog : MonoBehaviour {
+
+    [SerializeField] private string newScenePwd;
+    
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            PlayerController.Instance.scenePwd = newScenePwd;
+            this.gameObject.SetActive(false); 
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
